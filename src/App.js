925401e54042c4fc594d5770c478c52d4ead1d3d
BinTruong <bintruong97@gmail.com>
2020-12-11 16:52:34 +0700

    React Router

--- src/App.js
+++ src/App.js
@@ -3,13 +3,25 @@ import AboutUs from "./pages/AboutUs";
 //? Import Style
 import GlobalStyle from "./components/GlobalStyle";
 import Nav from "./components/Nav";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+//? Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Route path="/" exact>
+        <AboutUs />
+      </Route>
+      <Route path="/work">
+        <OurWork />
+      </Route>
+      <Route path="/contact">
+        <ContactUs />
+      </Route>
     </div>
   );
 }
