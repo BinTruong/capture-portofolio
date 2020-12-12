3a6146685566ae8c0c9cefd74decce14e39c9792
BinTruong <bintruong97@gmail.com>
2020-12-12 19:13:35 +0700

    Framer Motion

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,10 +1,18 @@
+//Animations
+import { motion } from "framer-motion";
+import { pageAnimation } from "../animation";
+
 const ContactUs = () => {
-    return (
-      <div>
-        <h1>Contact Us</h1>
-      </div>
-    );
-  };
-  
-  export default ContactUs;
-  
+  return (
+    <motion.div
+      variants={pageAnimation}
+      initial="hidden"
+      animate="show"
+      exit="exit"
+    >
+      <h1>Contact Us</h1>
+    </motion.div>
+  );
+};
+
+export default ContactUs;
